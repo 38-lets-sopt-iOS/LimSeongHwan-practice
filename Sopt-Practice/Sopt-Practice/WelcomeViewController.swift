43b7422a8cd0a53e5283c97c109c8cf93360d4fc
limtjdghks <epaktldk0203@gmail.com>
2026-04-09 21:59:49 +0900

    feat: 폰트스타일 정의 및 컴포넌트 폰트 적용

--- Sopt-Practice/Sopt-Practice/WelcomeViewController.swift
+++ Sopt-Practice/Sopt-Practice/WelcomeViewController.swift
@@ -22,6 +22,7 @@ class WelcomeViewController: UIViewController {
         label.text = "???님\n 반가워요!"
         label.textAlignment = .center
         label.numberOfLines = 2
+        label.font = .head1
 
         return label
     }()
@@ -31,7 +32,7 @@ class WelcomeViewController: UIViewController {
         button.setTitle("메인으로", for: .normal)
         button.backgroundColor = .orange
         button.titleLabel?.textColor = .white
-        button.titleLabel?.font = .boldSystemFont(ofSize: 18)
+        button.titleLabel?.font = .subHead1
         button.titleLabel?.textAlignment = .center
         button.layer.cornerRadius = 6
         
@@ -43,7 +44,7 @@ class WelcomeViewController: UIViewController {
         button.setTitle("다시 로그인", for: .normal)
         button.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
         button.titleLabel?.textColor = UIColor(red: 172/255, green: 176/255, blue: 185/255, alpha: 1)
-        button.titleLabel?.font = .boldSystemFont(ofSize: 18)
+        button.titleLabel?.font = .subHead1
         button.titleLabel?.textAlignment = .center
         button.layer.cornerRadius = 6
         
