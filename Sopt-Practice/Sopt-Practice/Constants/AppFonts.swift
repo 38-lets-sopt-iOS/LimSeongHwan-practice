7a563af250f9ddd40dae57c7c2e29a73c13d420d
limtjdghks <epaktldk0203@gmail.com>
2026-04-09 21:59:49 +0900

    feat: 폰트스타일 정의 및 컴포넌트 폰트 적용

---
+++ Sopt-Practice/Sopt-Practice/Constants/AppFonts.swift
@@ -0,0 +1,18 @@
+//
+//  AppFonts.swift
+//  Sopt-Practice
+//
+//  Created by 성환 on 4/9/26.
+//
+
+import UIKit
+
+extension UIFont {
+    static let head1 = UIFont.pretendard(size: 25, weight: .heavy)
+    static let subHead1 = UIFont.pretendard(size: 18, weight: .semibold)
+    static let subHead2 = UIFont.pretendard(size: 16, weight: .regular)
+    static let subHead3 = UIFont.pretendard(size: 15, weight: .bold)
+    static let subHead4 = UIFont.pretendard(size: 14, weight: .semibold)
+    static let subHead5 = UIFont.pretendard(size: 14, weight: .regular)
+    static let info = UIFont.pretendard(size: 12, weight: .regular)
+}
