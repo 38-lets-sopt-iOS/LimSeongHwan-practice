b29552af9c0647012c89684c1a79b6a8bb791048
SeongHwan <epaktldk0203@gmail.com>
2026-04-22 22:21:05 +0900

    feat: 오토레이아웃 실습

--- Sopt-Practice/Sopt-Practice/week02/AutoLayout_Practice.swift
+++ Sopt-Practice/Sopt-Practice/week02/AutoLayout_Practice.swift
@@ -38,10 +38,7 @@ class AutoLayout_Practice: UIViewController {
     }()
     
     func setUI() {
-        view.addSubview(yellowBox)
-        view.addSubview(greenBox)
-        view.addSubview(blackBox)
-        view.addSubview(blueBox)
+        view.addSubviews(yellowBox, greenBox, blackBox, blueBox)
         
         yellowBox.translatesAutoresizingMaskIntoConstraints = false
         greenBox.translatesAutoresizingMaskIntoConstraints = false
@@ -53,7 +50,17 @@ class AutoLayout_Practice: UIViewController {
         NSLayoutConstraint.activate([yellowBox.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
                                      yellowBox.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25)])
         NSLayoutConstraint.activate([greenBox.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
-                                     greenBox.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25),])
+                                     greenBox.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25),
+                                     greenBox.topAnchor.constraint(equalTo: yellowBox.bottomAnchor),
+                                     greenBox.leadingAnchor.constraint(equalTo: yellowBox.trailingAnchor)])
+        NSLayoutConstraint.activate([blackBox.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+                                     blackBox.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25),
+                                     blackBox.topAnchor.constraint(equalTo: greenBox.bottomAnchor),
+                                     blackBox.trailingAnchor.constraint(equalTo: greenBox.leadingAnchor)])
+        NSLayoutConstraint.activate([blueBox.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+                                     blueBox.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25),
+                                     blueBox.leadingAnchor.constraint(equalTo: blackBox.trailingAnchor),
+                                     blueBox.topAnchor.constraint(equalTo: blackBox.bottomAnchor)])
     }
     
     override func viewDidLoad() {
